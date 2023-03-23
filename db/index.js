2384897b732915a0d91b03f260a6cc7945c2f363
Md Ariful Islam <arifcse209@gmail.com>
2023-03-23 18:09:15 +0600

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose');
 mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGO_URI).then(()=>{
+mongoose.connect('mongodb+srv://arifcse209:eVmzXVPxS1W1hTNK@cluster0.yqdjk4p.mongodb.net/?retryWrites=true&w=majority').then(()=>{
     console.log("Database Connected");
 }).catch(err=>{
     console.log("Error connecting to database",err);
